dc7e9913ecdb5ee00aac10034273afc8ef4c8388
Doan Bui <bxdoan93@gmail.com>
2023-05-17 18:51:00 +0700

    check wifi available

--- macwifi/macwifi.py
+++ macwifi/macwifi.py
@@ -2,6 +2,8 @@ import subprocess
 import sys
 
 PATH_OF_AIRPORT = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+PATH_OF_NETWORK_SETUP = '/usr/sbin/networksetup'
+
 
 if "darwin" not in sys.platform:
     raise Exception("macwifi only works on macOS.")
@@ -47,16 +49,25 @@ def get_rssi():
     return output["agrCtlRSSI"]
 
 
-def connect(ssid, password):
+def is_wifi_available(ssid: str) -> bool:
+    """Check if the given SSID is available."""
+    return ssid in list()
+
+
+def connect(ssid : str, password : str, device : str = "en0") -> bool or Exception:
     """Connect to a WiFi network.
 
     Args:
         ssid: The SSID of the WiFi.
         password: The password of the WiFi.
+        device: The device name of the WiFi. Default is "en0".
     """
+    if not is_wifi_available(ssid):
+        raise Exception("The SSID is incorrect.")
+
     print(f"Connecting to {ssid}...")
     process = subprocess.Popen(
-        ["networksetup", "-setairportnetwork", "en0", ssid, password],
+        [PATH_OF_NETWORK_SETUP, "-setairportnetwork", device, ssid, password],
         stdout=subprocess.PIPE,
     )
     out, err = process.communicate()
@@ -71,7 +82,7 @@ def connect(ssid, password):
 def turn_on():
     """Turn on the WiFi."""
     process = subprocess.Popen(
-        ["networksetup", "-setnetworkserviceenabled", "Wi-Fi", "on"],
+        [PATH_OF_NETWORK_SETUP, "-setnetworkserviceenabled", "Wi-Fi", "on"],
         stdout=subprocess.PIPE,
     )
     out, err = process.communicate()
@@ -86,7 +97,7 @@ def turn_on():
 def turn_off():
     """Turn off the WiFi."""
     process = subprocess.Popen(
-        ["networksetup", "-setnetworkserviceenabled", "Wi-Fi", "off"],
+        [PATH_OF_NETWORK_SETUP, "-setnetworkserviceenabled", "Wi-Fi", "off"],
         stdout=subprocess.PIPE,
     )
     out, err = process.communicate()
